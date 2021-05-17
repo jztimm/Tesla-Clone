e13cdf47a157441b6a89d89780fb4c4133a57ff7
jztimm <jztimm@gmail.com>
2021-05-17 13:30:22 -0400

    Updated sections to have 2 buttons as well as animated arrowDown

--- src/components/Section.js
+++ src/components/Section.js
@@ -9,8 +9,18 @@ const Section = () => {
         <p>Order Online for Touchless Delivery</p>
       </ItemText>
 
+      <Buttons>
+        <ButtonGroup>
+          <LeftButton>
+            Custom Order
+          </LeftButton>
 
-      
+          <RightButton>
+            Existing Inventory
+          </RightButton>
+        </ButtonGroup>
+        <DownArrow src="/images/down-arrow.svg"/>
+      </Buttons>
     </Wrap>
   )
 }
@@ -22,11 +32,49 @@ const Wrap = styled.div`
   background-position: center;
   background-repeat: no-repeat;
   background-image: url('/images/model-s.jpg');
+  display: flex;
+  flex-direction: column;   // Switches alignments
+  justify-content: space-between;   // vertical alignment
+  align-items: center;    // horizontal alignment
 `
 
 const ItemText = styled.div`
   padding-top: 15vh;
   text-align: center;
+`
+
+const ButtonGroup = styled.div`
+  display: flex;
+  margin-bottom: 30px;
+`
+
+const LeftButton = styled.div`
+  background-color: rgba(23, 26, 32, 0.8);
+  height: 40px;
+  width: 256px;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 100px;
+  opacity: 0.85;
+  text-transform: uppercase;
+  font-size: 12px;
+  cursor: pointer;
+  margin: 8px
+`
+
+// Allows us to inherit the css of the LeftButton
+const RightButton = styled(LeftButton)``
+
+const DownArrow = styled.img`
+  margin-top: 20px;
+  height: 40px;
+  animation: animateDown infinite 1.5s;
+`
+
+const Buttons = styled.div`
+
 `
 
 export default Section
